9966a501db3c249d724d76dd6136f099a900f4bd
Awoyalejohn <awoyalejohn@gmail.com>
2022-05-26 18:14:25 +0000

    Add Post model with attributes

--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """ Post model to create instances of posts """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name=post_author)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name=post_topic)
+    last_updated = models.DateField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name=post_likes, blank=True)
+
