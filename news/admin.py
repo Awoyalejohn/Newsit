6a3ec02270e661f0d1eabea2ed48000478d17c5d
Awoyalejohn <awoyalejohn@gmail.com>
2022-05-27 08:58:47 +0000

    Add comment to admin page and more admin functionality

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Post, Topic
+from .models import Post, Topic, Comment
 
 # Register your models here.
 admin.site.register(Topic)
@@ -9,8 +9,16 @@ admin.site.register(Topic)
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     """ Admin interface for posts """
-    list_display = ('title', 'slug', 'topic', 'created_on')
+    list_display = ('title', 'slug', 'topic', 'created_on', 'last_updated')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('topic', 'created_on')
     summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+    """ Admin interface for comments """
+    list_display = ('user', 'body', 'post', 'created_on')
+    list_filter = ['created_on']
+    search_fields = ('user', 'body')
