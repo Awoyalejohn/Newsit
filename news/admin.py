b01f36047eda1e5ace7e8efc89d116b992dabb5e
Awoyalejohn <awoyalejohn@gmail.com>
2022-05-27 08:39:14 +0000

    Add topic to admin page

--- news/admin.py
+++ news/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post, Topic
 
 # Register your models here.
+admin.site.register(Topic)
+
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     """ Admin interface for posts """
-    list_display = ('title', 'slug', 'created_on')
+    list_display = ('title', 'slug', 'topic', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('topic','created_on')
+    list_filter = ('topic', 'created_on')
     summernote_fields = ('content')
-
--- news/models.py
+++ news/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """ Topic model to create categories for posts """
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.name
