275918619f2cd1ca8620528ccb706653637aa090
Awoyalejohn <awoyalejohn@gmail.com>
2022-05-28 07:36:17 +0000

    Add commentform class in forms.py

--- news/forms.py
+++ news/forms.py
@@ -4,9 +4,16 @@ from .models import Post, User
 
 
 class PostForm(forms.ModelForm):
-    """ Class for making forms in Django """
+    """ Class for making forms posts"""
     class Meta:
         model = Post
         fields = ("topic", "title", "content",)
+
+
+class CommentForm(forms.ModelForm):
+    """ Class for making forms posts"""
+    class Meta:
+        model = Comment
+        fields = ('body',)
 
     
