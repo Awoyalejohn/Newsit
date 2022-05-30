bc713e3b73691d0ac64cb04083af6a512a2c1f01
Awoyalejohn <awoyalejohn@gmail.com>
2022-05-30 00:30:09 +0000

    Add styles to forms and responsive design

--- news/forms.py
+++ news/forms.py
@@ -9,11 +9,23 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ("topic", "title", "content",)
 
+        widgets = {
+            "topic": forms.Select(attrs={'class': 'form-control'}),
+            "title": forms.TextInput(attrs={'class': 'form-control'}),
+            "content": forms.Textarea(attrs={'class': 'form-control'}),
+
+        }
+
 
 class CommentForm(forms.ModelForm):
     """ Class for making forms posts"""
     class Meta:
         model = Comment
         fields = ('body',)
+
+        widgets = {
+            "body": forms.Textarea(attrs={'class': 'form-control'}),
+
+        }
 
     
